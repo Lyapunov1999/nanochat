f7c60351f5e2d3dfff6033a29fb57ab663159908
ych <1598615521@qq.com>
2026-04-28 12:52:38 -0400

    Adjust time limits

--- runs/slurm/speedrun.sh
+++ runs/slurm/speedrun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=nanochat-speedrun
 #SBATCH --account=rrg-schmidtm
-#SBATCH --time=00:01:00
+#SBATCH --time=02:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
